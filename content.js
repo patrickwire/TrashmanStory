4c5ff41fd52ad68be0db6a3be9572b89c2f45fba
Rene Heinrich <mail@reneheinrich.de>
2016-02-20 08:23:12 +0100

    null testing for links

--- content.js
+++ content.js
@@ -1,7 +1,10 @@
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse){
 	if(msg.text === 'report_back') {
 		//sendResponse(document.all[0].outerHTML);
-		var randomLink = Math.floor(Math.random() * document.getElementsByTagName("a").length);
-		sendResponse(document.getElementsByTagName("a")[randomLink].href);
+		var links = document.getElementsByTagName("a");
+		if (links != null && links.length > 0) {
+			var randomIndex = Math.floor(Math.random() * links.length);
+			sendResponse(links[randomIndex].href);
+		}
 	}
 });
